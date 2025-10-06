ff71b16e908dac70bb756bb92cee5c37732a97d0
pavankotha <pavankotha9550@outlook.com>
2025-10-06 15:41:19 +0530

    concepts mail

---
+++ memory.sh
@@ -0,0 +1,15 @@
+#!/bim/bash
+
+usage=$( df -hT | grep -v Filesystem |awk '{print $6F}' |cut -d "%" -f1)
+msg=''
+
+while IFS=read line
+do
+    if [ $line -gt 1 ]
+    then
+        mount=$(grep -v Filesystem |awk '{print $7F}')
+        msg+= $mount: $usage
+    fi
+done >>>$usage
+
+echo -e "high memory utilization on $msg \n"
