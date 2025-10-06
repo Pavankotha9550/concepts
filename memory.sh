542a4740d362bff0124ddd0691ef5342be2b9615
pavankotha <pavankotha9550@outlook.com>
2025-10-06 16:36:21 +0530

    concepts mail

--- memory.sh
+++ memory.sh
@@ -1,16 +1,18 @@
 #!/bim/bash
 
-usage=$( df -hT | grep -v Filesystem |awk '{print $6F}' |cut -d "%" -f1)
+usage=$( df -hT | grep -v Filesystem )
 msg=''
 
 while IFS= read -r line
 do
   if [ $line -ge 0 ]
   then
-    mount=$(df -hT | grep -v Filesystem |awk '{print $7F}')
+    num=$($line |awk '{print $6F}' |cut -d "%" -f1)
+    mount=$($line |awk '{print $7F}')
     msg+="hight on $mount : $line \n"
   fi 
 done <<< $usage
 
 echo $msg
 
+#df -hT | grep -v Filesystem |awk '{print $6F}' |cut -d "%" -f1
