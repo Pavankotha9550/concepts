2fb621f80b98cffdd8a25c329f45ea9c4d49fc43
pavankotha <pavankotha9550@outlook.com>
2025-10-07 15:08:02 +0530

    concepts mail

--- memory.sh
+++ memory.sh
@@ -2,6 +2,7 @@
 
 usage=$( df -hT | grep -v Filesystem )
 msg=''
+IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 while IFS= read -r line
 do
@@ -9,10 +10,12 @@ do
     mount=$(echo $line |awk '{print $7F}')
     if [ $num -gt 0 ]
     then
-    msg+="hight on $mount : $num \n"
+    msg+="high on $mount : $num \n"
   fi 
 done <<< $usage
 
 echo -e $msg
 
 #df -hT | grep -v Filesystem |awk '{print $6F}' |cut -d "%" -f1
+
+sh mailtemplate.sh "DevOps Team" "High Disk Usage" "$IP" "$msg" "info@joindevops.com" "ALERT-High Disk Usage"
